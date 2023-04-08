1a3d7a7dd10e537c392cf3da311eeae5f50b90ab
Nisha Singhal <nishasinghal979@gmail.com>
2023-04-08 19:36:16 +0530

    Update squares with X when clicked

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 
-function Square({val}){
+function Square({val, onSquareCLick}){
   
-  return <button className="square" >{val}</button>;
+  return <button className="square" onClick={onSquareCLick}>{val}</button>;
   
 }
 
@@ -11,23 +11,29 @@ export default function Board() {
 
   const [squares, setSquares] = useState(Array(9).fill(null));
 
+  function handleClick(i){
+    const nextSquares = squares.slice();
+    nextSquares[i] = 'X';
+    setSquares(nextSquares);
+  }
+
   return (
-  <>
-  <div className='board-row'>
-  <Square val={squares[0]}/>
-  <Square val={squares[1]}/>
-  <Square val={squares[2]}/>
-  </div>
-  <div className='board-row'>
-  <Square val={squares[3]}/>
-  <Square val={squares[4]}/>
-  <Square val={squares[5]}/>
-  </div>
-  <div className='board-row'>
-  <Square val={squares[6]}/>
-  <Square val={squares[7]}/>
-  <Square val={squares[8]}/>
-  </div>
+    <>
+      <div className='board-row'>
+      <Square val={squares[0]} onSquareCLick={()=>handleClick(0)}/>
+      <Square val={squares[1]} onSquareCLick={()=>handleClick(1)}/>
+      <Square val={squares[2]} onSquareCLick={()=>handleClick(2)}/>
+      </div>
+      <div className='board-row'>
+      <Square val={squares[3]} onSquareCLick={()=>handleClick(3)}/>
+      <Square val={squares[4]} onSquareCLick={()=>handleClick(4)}/>
+      <Square val={squares[5]} onSquareCLick={()=>handleClick(5)}/>
+      </div>
+      <div className='board-row'>
+      <Square val={squares[6]} onSquareCLick={()=>handleClick(6)}/>
+      <Square val={squares[7]} onSquareCLick={()=>handleClick(7)}/>
+      <Square val={squares[8]} onSquareCLick={()=>handleClick(8)}/>
+      </div>
   
-  </>);
+    </>);
 }
