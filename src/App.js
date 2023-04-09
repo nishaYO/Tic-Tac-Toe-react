cf1869b6a2a4eac3631a4201855c0af2f9079bdc
Nisha Singhal <nishasinghal979@gmail.com>
2023-04-09 09:32:50 +0530

    Add X, O and winner declare function

--- src/App.js
+++ src/App.js
@@ -6,19 +6,39 @@ function Square({val, onSquareCLick}){
   
 }
 
-
-export default function Board() {
+function Board() {
 
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [XIsNext, setXIsNext] = useState(true);
 
   function handleClick(i){
+    
+    if(squares[i] || calcWinner(squares)){
+      return;
+    }
     const nextSquares = squares.slice();
-    nextSquares[i] = 'X';
+    if(XIsNext){
+      nextSquares[i] = 'X';
+    }
+    else{
+      nextSquares[i] = 'O';
+    }
     setSquares(nextSquares);
-  }
+    setXIsNext(!XIsNext);
+    }
+    let status;
+    const winner = calcWinner(squares);
+    if(winner){
+      status = "Winner: " + winner;
+    }
+    else{
+      status = "Next: " + (XIsNext ? "X" : "O");
+    }
+  
 
   return (
     <>
+      <div className="status">{status}</div>
       <div className='board-row'>
       <Square val={squares[0]} onSquareCLick={()=>handleClick(0)}/>
       <Square val={squares[1]} onSquareCLick={()=>handleClick(1)}/>
@@ -36,4 +56,41 @@ export default function Board() {
       </div>
   
     </>);
+
+    function calcWinner(squares){
+      const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+      ];
+
+      for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+          return squares[a];
+        }
+      }
+      return null;
+    }
+}
+
+
+export default function Game(){
+    return (
+      <>
+      <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>      
+      </>
+    )
 }
