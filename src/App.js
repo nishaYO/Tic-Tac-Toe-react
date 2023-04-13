b047c6ccef831493755971521ecd2940a723ce3e
Nisha Singhal <nishasinghal979@gmail.com>
2023-04-13 09:03:39 +0530

    Enhanced Board Component using for loop

--- src/App.js
+++ src/App.js
@@ -37,27 +37,21 @@ function Board({ xIsNext, squares, onPlay }) {
   } else {
     status = 'Next player: ' + (xIsNext ? 'X' : 'O');
   }
-  
+
+  // Rewrite Board to use two loops to make the squares instead of hardcoding them.
+  let boardSquares = [<div className="status">{status}</div>];
+  for (let i = 0; i < 3; i++) {
+    let row = [];
+    for (let j = 0; j < 3; j++) {
+      const index = i * 3 + j;
+      row.push(<Square value={squares[index]} onSquareClick={() => handleClick(index)} />);
+    }
+    boardSquares.push(<div className="board-row">{row}</div>);
+  }
   //JSX to create 9 squares in the boxes
   return (
     <>
-      <div className="status">{status}</div>
-      
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {boardSquares}
     </>
   );
 }
